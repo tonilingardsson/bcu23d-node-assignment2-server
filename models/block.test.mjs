980a28a8e7f6224a39cbcab888969d6b058c71e5
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-06-13 08:54:30 +0200

    Create Block's test.

---
+++ models/block.test.mjs
@@ -0,0 +1,10 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+
+describe('Block', () => {
+    const timestamp = Date.now();
+
+    it('should return ans instance of Block', () => {
+        // const block = new Block({timestamp});
+        // expect(block.timestamp).toEqual(timestamp);
+    });
+})
