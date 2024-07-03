fc578ca3baad09235d901cb0faabbd6592bb34b4
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-03 11:30:39 +0200

    First transactions tests

--- models/Transaction.mjs
+++ models/Transaction.mjs
@@ -1,2 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 import { verifySignature } from '../utilities/crypto-lib.mjs';
+
+export default class Transaction {
+    constructor(sender, recipient, amount) {
+        this.id = 1;
+        this.outputMap = {};
+        this.inputMap = {};
+    }
+}
--- tests/transaction.test.mjs
+++ tests/transaction.test.mjs
@@ -1,7 +1,32 @@
 import { it, describe, expect, beforeEach } from 'vitest';
-import Transaction from '../models/Transaction.mjs';
+import Wallet from '../models/Wallet.mjs';
 import { verifySignature } from '../utilities/crypto-lib.mjs';
+import Transaction from '../models/Transaction.mjs';
 
 describe('Transaction', () => {
     let transaction, sender, recipient, amount;
-})
+
+    beforeEach(() => {
+        // We initiate our sender with new Wallet().
+        // For testing, we get a new fresh wallet
+        sender = new Wallet();
+        recipient = 'recipient-dummy-address';
+        amount = 50;
+
+        transaction = new Transaction({ sender, recipient, amount })
+    });
+
+    describe('Properties', () => {
+        it('should have a property named id', () => {
+            expect(transaction).toHaveProperty('id');
+        })
+
+        it('should have a property named outputMap', () => {
+            expect(transaction).toHaveProperty('outputMap');
+        })
+
+        it('should have a property named inputMap', () => {
+            expect(transaction).toHaveProperty('inputMap');
+        });
+    });
+});
