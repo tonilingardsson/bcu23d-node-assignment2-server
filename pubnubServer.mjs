abd152c0ec66840a7b9f0907cbb166dda597465a
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-08 12:24:02 +0200

    Add transaction to pubnubServer

--- pubnubServer.mjs
+++ pubnubServer.mjs
@@ -1,55 +1,19 @@
 import Pubnub from 'pubnub';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './config/config.env' });
 
 const CHANNELS = {
     DEMO: 'DEMO',
-    BLOCKCHAIN: 'BLOCKCHAIN'
+    BLOCKCHAIN: 'BLOCKCHAIN',
+    TRANSACTION: 'TRANSACTION',
 };
 
-// I am using a .env file to store the Pubnub credentials.
-const pubnubCredentials = {
-    publishKey: process.env.PUBLISH_KEY,
-    subscribeKey: process.env.SUBSCRIBE_KEY,
-    secretKey: process.env.SECRET_KEY,
-    userId: process.env.USER_ID
-};
 export default class PubnubServer {
-    // This constructor takes in our blockchain as a parameter, object destructuring. 
-    // And importing Pubnub.
-    constructor({ blockchain, credentials }) {
+    constructor({ blockchain, credentials, transactionPool, wallet }) {
         this.blockchain = blockchain;
-        // This is our Pubnub instance. It will connect the app to the cloud.
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
         this.pubnub = new Pubnub(credentials);
-        // Set up the subscription to our channel. Pubnub makes it easy to create channels.
         this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
-        // Listen for messages on our channel. It takes in a callback function.
         this.pubnub.addListener(this.listener());
-        // Set up the listener method. As an object that returns a message method.
-    }
-    listener() {
-        return {
-            message: msgObject => {
-                const { channel, message } = msgObject;
-                // The message is a string. We need to parse it to an object.
-                const msg = JSON.parse(message);
-                console.log(`Message received. Channel: ${channel} Message: ${message}.`);
-
-                // If the message is a block, add it to the blockchain.
-                if (channel === CHANNELS.BLOCKCHAIN) {
-                    this.blockchain.replaceChain(msg);
-                }
-            },
-        };
-    }
-    // We need a publish and a broadcast method.
-    subscribeToChannels() { }
-
-    // Publish is the method that will publish messages that the listener will receive.
-    publish({ channel, message }) {
-        // It uses Pubnub's publish method, which sends a message to a channel. This will be received by the listener.
-        this.pubnub.publish({ channel, message });
     }
 
     broadcastChain() {
@@ -60,6 +24,43 @@ export default class PubnubServer {
         });
     }
 
+    broadcastTransaction(transaction) {
+        this.publish({
+            channel: CHANNELS.TRANSACTION,
+            message: JSON.stringify(transaction),
+        });
+    }
+    listener() {
+        return {
+            message: (msgObject) => {
+                const { channel, message } = msgObject;
+                const msg = JSON.parse(message);
+                console.log(`Message received. Channel: ${channel} Message: ${message}.`);
 
+                // If the message is a block, add it to the blockchain.
+                // if (channel === CHANNELS.BLOCKCHAIN) {
+                //     this.blockchain.replaceChain(msg);
+                // }
+                // We replace the if statement with the following.
+                switch (channel) {
+                    case CHANNELS.BLOCKCHAIN:
+                        this.blockchain.replaceChain(msg);
+                        break;
+                    case CHANNELS.TRANSACTION:
+                        if (!this.transactionPool.existingTransaction({ address: this.wallet.publicKey, })
+                        ) {
+                            this.transactionPool.addTransaction(msg);
+                        }
+                        break;
+                    default:
+                        return;
+
+                }
+            },
+        };
+    }
+
+    publish({ channel, message }) {
+        this.pubnub.publish({ channel, message });
+    }
 }
-
