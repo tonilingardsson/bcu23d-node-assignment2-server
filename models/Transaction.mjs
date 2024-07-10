6e03743b1abeba7e899dffa7153792b469940b96
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-10 11:14:31 +0200

    Update Transaction.mjs with REWARD_ADDRESS and MINING_REWARD

--- models/Transaction.mjs
+++ models/Transaction.mjs
@@ -1,30 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 import { verifySignature } from '../utilities/crypto-lib.mjs';
+import { REWARD_ADDRESS, MINING_REWARD } from '../config/settings.mjs';
 
 export default class Transaction {
-    constructor({ sender, recipient, amount }) {
+    constructor({ sender, recipient, amount, outputMap, inputMap }) {
         this.id = uuidv4().replaceAll('-', '');
-        this.outputMap = this.createOutputMap({ sender, recipient, amount });
-        this.inputMap = this.createInputMap({ sender, outputMap: this.outputMap });
+        this.outputMap =
+            outputMap || this.createOutputMap({ sender, recipient, amount });
+        this.inputMap =
+            inputMap || this.createInputMap({ sender, outputMap: this.outputMap });
     }
 
-    // createOutputMap({ sender, recipient, amount }) {
-    //     const outputMap = {};
-
-    //     outputMap[recipient] = amount;
-    //     outputMap[sender.publicKey] = sender.balance - amount;
-
-    //     return outputMap;
-    // }
-
-    // createInputMap({ sender, outputMap }) {
-    //     return {
-    //         timestamp: Date.now(),
-    //         amount: sender.balance,
-    //         address: sender.publicKey,
-    //         signature: sender.sign(outputMap),
-    //     };
-    // }
+    // Reward transaction
+    static transactionReward({ miner }) {
+        return new this({
+            inputMap: REWARD_ADDRESS,
+            outputMap: { [miner.publicKey]: MINING_REWARD },
+        });
+    }
 
     // Static methods...
     static validate(transaction) {
@@ -35,26 +28,31 @@ export default class Transaction {
         // const {outputMap} = transaction;
         // const {address, amount, signature} = transaction.inputMap;
 
-        const outputTotal = Object.values(outputMap).reduce((total, amount) =>
-            total + amount
+        const outputTotal = Object.values(outputMap).reduce(
+            (total, amount) => total + amount
         );
 
         if (amount !== outputTotal) return false;
 
-        if (!verifySignature({ publicKey: address, data: outputMap, signature })) return false;
+        if (!verifySignature({ publicKey: address, data: outputMap, signature }))
+            return false;
 
         return true;
     }
 
     update({ sender, recipient, amount }) {
-        console.log('INPUT MAP', this.inputMap);
-        console.log('SENDER OUTPUTMAP', this.outputMap);
-        if (amount > this.outputMap[sender.publicKey]) throw new Error('Not enough funds!');
+        if (amount > this.outputMap[sender.publicKey])
+            throw new Error('Not enough funds!');
 
-        this.outputMap[recipient] = amount;
-        console.log('Amount: ', this.outputMap[sender.publicKey]);
+        // Create an output map if the recipient doesn't already have one. Otherwise, add the amount to it.
+        if (!this.outputMap[recipient]) {
+            this.outputMap[recipient] = amount;
+        } else {
+            this.outputMap[recipient] = this.outputMap[recipient] + amount;
+        }
 
-        this.outputMap[sender.publicKey] = this.outputMap[sender.publicKey] - amount;
+        this.outputMap[sender.publicKey] =
+            this.outputMap[sender.publicKey] - amount;
 
         this.inputMap = this.createInputMap({ sender, outputMap: this.outputMap });
     }
