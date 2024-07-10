c9cc5dc175e655e48cda91ee0ecbed3e78481c79
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-10 11:16:04 +0200

    Update TransactionPool.mjs with clearBlockTransactions and validateTransactions

--- models/TransactionPool.mjs
+++ models/TransactionPool.mjs
@@ -2,16 +2,47 @@ import Transaction from './Transaction.mjs';
 
 export default class TransactionPool {
     constructor() {
-        this.transactionMap = [];
+        this.transactionMap = {};
     }
     addTransaction({ transaction }) {
         this.transactionMap[transaction.id] = transaction;
     }
+
+    clearBlockTransactions({ chain }) {
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+
+            for (let transaction of block.data) {
+                if (this.transactionMap[transaction.id]) {
+                    delete this.transactionMap[transaction.id];
+                }
+            }
+        }
+    }
+
+
+    clearTransactions() {
+        this.transactionMap = {};
+    }
+
+    replaceTransactionMap(transactionMap) {
+        this.transactionMap = transactionMap;
+    }
+
+
+
     existingTransaction({ address }) {
         const transactions = Object.values(this.transactionMap);
 
         return transactions.find(
             (transaction) => transaction.inputMap.address === address
         );
+    }
+
+    validateTransactions() {
+        const validTransactions = Object.values(this.transactionMap).filter(
+            (transaction) => Transaction.validate(transaction)
+        );
+        return validTransactions;
     }
 }
