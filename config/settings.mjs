d85e29b55434241bd4477bb619eedfb64f76db1a
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-10 11:08:34 +0200

    Add REWARD_ADDRESS AND MINING_REWARD on settings.mjs

--- config/settings.mjs
+++ config/settings.mjs
@@ -1,5 +1,7 @@
 export const MINE_RATE = 1000;// 1000 ms = 1 second
 export const INITIAL_BALANCE = 1000;
+export const REWARD_ADDRESS = { address: 'reward-address' };
+export const MINING_REWARD = 50;
 const INITIAL_DIFFICULTY = 3;
 
 export const GENESIS_DATA = {
