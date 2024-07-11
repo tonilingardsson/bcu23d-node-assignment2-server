83112f8c0ab2674fa61db6126ec1e80f03badce8
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-11 08:05:30 +0200

    Update status: true on blockchain controller

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -3,5 +3,5 @@ import { blockchain } from '../server.mjs';
 export const listBlock = (req, res, next) => {
     res
         .status(200)
-        .json({ status: 'success', statusCode: 200, data: blockchain.chain });
+        .json({ status: true, statusCode: 200, data: blockchain.chain });
 };
