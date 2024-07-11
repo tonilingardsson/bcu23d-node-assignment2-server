8304a5327de237ad746ff85aff27386536f56509
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-11 08:57:13 +0200

    Update Wallet with calculateBalance

--- models/Wallet.mjs
+++ models/Wallet.mjs
@@ -6,16 +6,48 @@ export default class Wallet {
     constructor() {
         this.balance = INITIAL_BALANCE;
         this.keyPair = ellipticHash.genKeyPair();
-        // This is our address
         this.publicKey = this.keyPair.getPublic().encode('hex');
     }
 
+    static calculateBalance({ chain, address }) {
+        let total = 0;
+        let hasAddedTransaction = false;
+
+        // Go through every block in the chain
+        for (let i = chain.length - 1; i > 0; i--) {
+            const block = chain[i];
+
+            // For every block, go through every transaction...
+            for (let transaction of block.data) {
+                if (transaction.inputMap.address === address) {
+                    hasAddedTransaction = true;
+                }
+                const value = transaction.outputMap[address];
+
+                if (value) {
+                    total += value;
+                }
+            }
+
+            if (hasAddedTransaction) break;
+        }
+
+        return hasAddedTransaction ? total : INITIAL_BALANCE + total;
+    }
+
+    createTransaction({ recipient, amount, chain }) {
+        if (chain) {
+            this.balance = Wallet.calculateBalance({
+                chain,
+                address: this.publicKey,
+            });
+        }
+        if (amount > this.balance) throw new Error('Not enough funds!');
+        return new Transaction({ sender: this, recipient, amount });
+    }
     sign(data) {
         return this.keyPair.sign(createHash(data));
     }
 
-    createTransaction({ recipient, amount }) {
-        if (amount > this.balance) throw new Error('Not enough funds!');
-        return new Transaction({ sender: this, recipient, amount });
-    }
+
 }
