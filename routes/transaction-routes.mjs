ca47d1913b46b3918592c9c6c8ac9e66732e3ee1
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-11 08:59:46 +0200

    Update transaction-routes with getWalletBalance

--- routes/transaction-routes.mjs
+++ routes/transaction-routes.mjs
@@ -1,10 +1,16 @@
 import express from 'express';
-import { addTransaction, getTransactionPool, mineTransactions } from '../controllers/transaction-controller.mjs';
+import {
+    addTransaction,
+    getTransactionPool,
+    getWalletBalance,
+    mineTransactions,
+} from '../controllers/transaction-controller.mjs';
 
 const router = express.Router();
 
 router.route('/transaction').post(addTransaction);
 router.route('/transactions').get(getTransactionPool);
 router.route('/mine').get(mineTransactions);
+router.route('/info').get(getWalletBalance);
 
 export default router;
