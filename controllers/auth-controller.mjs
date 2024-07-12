85dcb8d04b89d43120c8bad4af0c567f40662805
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-12 11:03:49 +0200

    Register requires name, email and password. Otherwise: statusCode:400

--- controllers/auth-controller.mjs
+++ controllers/auth-controller.mjs
@@ -2,19 +2,28 @@
 // @route   POST /api/v1/auth/register
 // @access  PUBLIC
 export const register = (req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    if (!name || !email || !password) {
+        return res
+            .status(400)
+            .json({
+                status: false,
+                statusCode: 400,
+                error: 'Username, email or password missing!',
+            });
+    }
     res
         .status(201)
         .json({ status: true, statusCode: 201, data: 'Register success!' });
-}
+};
 
 // @desc    Login a user
 // @route   POST /api/v1/auth/login
 // @access  PUBLIC
 export const login = (req, res, next) => {
-    res
-        .status(200)
-        .json({ status: true, statusCode: 200, data: 'Login works!' });
-}
+    res.status(200).json({ status: true, statusCode: 200, data: 'Login works!' });
+};
 
 // @desc    Return info about a logged in user
 // @route   GET /api/v1/auth/me
@@ -23,4 +32,4 @@ export const getMe = (req, res, next) => {
     res
         .status(200)
         .json({ status: true, statusCode: 200, data: 'My profile works too!' });
-}
+};
