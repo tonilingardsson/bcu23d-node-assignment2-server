af95f3fbc60c3b2af9debdbc4eeee2e0cf1c985d
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-13 11:22:07 +0200

    Implement readFileAsync and update saveUser with loadUsers to enable fileHandler to load existing users

--- data/fileDb.mjs
+++ data/fileDb.mjs
@@ -1,8 +1,14 @@
-import { writeFileAsync } from "../utilities/fileHandler.mjs";
+import { readFileAsync, writeFileAsync } from "../utilities/fileHandler.mjs";
 
 export const saveUser = async (user) => {
-    const users = [];
+    const users = await loadUsers();
 
     users.push(user);
+
     await writeFileAsync('data', 'users.json', JSON.stringify(users));
+}
+
+const loadUsers = async () => {
+    // Write users to file if they don't exist yet
+    return (await readFileAsync('data', 'users.json'));
 }
