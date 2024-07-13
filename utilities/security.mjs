b2326a3431d9d2f82cd98d416cf91b96c3aeb53e
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-13 11:18:15 +0200

    Create fn generateToken on security using JWT

--- utilities/security.mjs
+++ utilities/security.mjs
@@ -1,4 +1,10 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (id) => {
+    // Create a token 
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TTL });
+}
 
 export const hashPassword = (password) => {
     console.log('password', password);
