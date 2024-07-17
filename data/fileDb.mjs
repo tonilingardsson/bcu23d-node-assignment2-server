3ec67574ef3102f9dd8b1e4f6957b61fd1a16ea1
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-17 15:57:04 +0200

    Update fileDb with findUserById

--- data/fileDb.mjs
+++ data/fileDb.mjs
@@ -4,15 +4,31 @@ export const findUserByEmail = async (email) => {
     const users = await loadUsers();
     // Convert to lowercase []
     const user = users.find((user) => user.email === email);
+
     if (!user) {
         throw new Error('User not found');
     }
+
     return {
         id: user.id,
         name: user.name,
         email: user.email,
         password: user.password,
-    }
+    };
+};
+
+export const findUserById = async (id) => {
+    const users = await loadUsers();
+    const user = users.find((user) => user.id === id);
+
+    if (!user) throw new Error('User not found');
+
+    return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+    };
 }
 
 export const saveUser = async (user) => {
@@ -25,5 +41,5 @@ export const saveUser = async (user) => {
 
 const loadUsers = async () => {
     // Write users to file if they don't exist yet
-    return (await readFileAsync('data', 'users.json'));
+    return await readFileAsync('data', 'users.json');
 }
