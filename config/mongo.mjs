1c6565bd890a087bf481f8e6ccf0f3914da486f7
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-18 10:42:46 +0200

    Create mongo.mjs with mongoose import

---
+++ config/mongo.mjs
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+export const connectDb = async () => {
+    const mongoConn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected to: ${mongoConn.connection.host}`.cyan.underline.bold);
+}
