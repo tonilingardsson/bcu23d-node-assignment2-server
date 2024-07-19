6d11579ec61deaa1fd3ad82820b0cb01cc7c393f
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-19 13:37:28 +0200

    Update UserModel for MongoDb schema

--- models/UserModel.mjs
+++ models/UserModel.mjs
@@ -1,13 +1,35 @@
 import { v4 as uuidv4 } from 'uuid';
 import { hashPassword } from '../utilities/security.mjs';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export default class User {
-    constructor(name, email, password, role) {
-        this.id = uuidv4().replaceAll('-', '');
-        this.name = name;
-        this.email = email;
-        this.password = hashPassword(password);
-        this.role = role;
-        this.createdAt = Date.now();
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: [true, 'Name is required'] },
+    email: { type: String, required: [true, 'Email is required'], unique: true, match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'] },
+    role: { type: String, enum: ['user', 'manager'], default: 'user' },
+    password: { type: String, required: [true, 'Password is required'] },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
     }
-}
+
+    this.password = await bcrypt.hash(this.password, 12);
+})
+
+userSchema.methods.validatePassword = async function (passwordToCheck) {
+    return await bcrypt.compare(passwordToCheck, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+    return jwt.sign({ id: this.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TTL });
+};
+
+
+export default mongoose.model('User', userSchema);
